8dfc46e6a3a6bf8106e13b9b6fd7964329162c81
KahhorovSh04 <shaxzodqaxxorov04@gmail.com>
2021-11-25 03:13:23 +0500

    #4 Features added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Courses from './components/Courses'
+import Features from './components/Features'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Header />
       <Courses />
+      <Features />
     </div>
   )
 }
---
+++ src/components/Features.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../css/features.css'
+
+const Features = () => {
+  return (
+    <article id='features'>
+      <h1 className='topic'>Features</h1>
+      <div className='container'>
+        <div className='box'>
+          <i className='fas fa-user'></i>
+          <p>
+            O'quvchilarni imtihonlarga tayyorlash bo'yicha zamonaviy
+            dunyo-qarashga ega ustozlar
+          </p>
+        </div>
+        <div className='box'>
+          <i className='fa-solid fa-graduation-cap'></i>
+          <p>Academic Support - ikkinchi o'qituvchi xizmati.</p>
+        </div>
+        <div className='box'>
+          <i className='fas fa-users'></i>
+          <p>
+            Darsdan tashqari qo'shimcha dars tayyorlash uchun co-working joylar
+          </p>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Features
