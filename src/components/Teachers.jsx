5ab17db236fc950cc8223ea20be6725160a78f30
KahhorovSh04 <shaxzodqaxxorov04@gmail.com>
2021-11-26 10:00:03 +0500

    Instructor replaced teacher

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -5,21 +5,21 @@ import TeacherBox from './TeacherBox'
 const teachers = [
   {
     name: 'John Doe',
-    description: 'Teacher from IELTS 7+ and 8.5 score holder',
+    description: 'Instructor of IELTS 7+ course and 8.5 score holder',
     banner: 'teacher1.png',
     background:
       'linear-gradient(180deg, rgba(0, 0, 0, 0) 48.17%, #0ACF83 92.48%)',
   },
   {
     name: 'Edward Snowden',
-    description: 'Teacher from General English and 8.0 holder',
+    description: 'Instructor of General English course and 8.0 holder',
     banner: 'teacher2.png',
     background:
       'linear-gradient(180deg, rgba(0, 0, 0, 0) 53.63%, #0AACDE 91.05%)',
   },
   {
-    name: 'Jone Doe',
-    description: 'Teacher from IELTS speed-up and 8.0 holder',
+    name: 'Jane Doe',
+    description: 'Instructor of IELTS speed-up course and 8.0 holder',
     banner: 'teacher3.png',
     background:
       'linear-gradient(180deg, rgba(0, 0, 0, 0) 53.19%, #F24E1E 91.59%)',
@@ -29,7 +29,7 @@ const teachers = [
 const Teachers = () => {
   return (
     <article id='teachers'>
-      <h2 className='topic'>Our Teachers</h2>
+      <h2 className='topic'>Our Instructors</h2>
       <div className='container'>
         {teachers.map((teacher, ind) => (
           <TeacherBox teacher={teacher} key={ind} />
